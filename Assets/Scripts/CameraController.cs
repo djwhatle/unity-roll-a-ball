ec650be87617695bdc2359afb0c2c814888953d3
Derek Whatley <derekwhatley@gmail.com>
2016-09-18 23:18:34 -0400

    initial commit, roll-a-ball complete

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	// Player object will be used for tracking info
+	public GameObject player;
+
+	// Offset will be set in the script
+	// We will take current transform position delta between camera and player to determine offset
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Camera is moved into new position inline with camera object
+		// Update is not the best place for this code
+		// For follow-cameras, procedural animation, gathering last-known state, it's better to gather late-update
+		transform.position = player.transform.position + offset;
+	}
+}
